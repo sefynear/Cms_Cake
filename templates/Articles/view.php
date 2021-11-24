d2c8e09aa96178e90dccc5a1cf0af69c8b115cdd
sefynear <sefynear@gmail.com>
2021-11-24 16:44:09 +0100

    Cms Cakephp

---
+++ templates/Articles/view.php
@@ -0,0 +1,6 @@
+<!-- Fichier : templates/Articles/view.php -->
+
+<h1><?= h($article->title) ?></h1>
+<p><?= h($article->body) ?></p>
+<p><small>Créé le : <?= $article->created->format(DATE_RFC850) ?></small></p>
+<p><?= $this->Html->link('Modifier', ['action' => 'edit', $article->slug]) ?></p>
